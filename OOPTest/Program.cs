385953621972e738872207238bbaa8185b69e700
MVA <itwins19@gmail.com>
2022-11-09 19:20:41 +0300

    new-car-list

--- OOPTest/Program.cs
+++ OOPTest/Program.cs
@@ -31,6 +31,129 @@ namespace OOPTest
 
             Console.ReadLine();
         }
-        
+
+        private static List<Scooter> GetScooters()
+        {
+            return new List<Scooter>
+            {
+                new Scooter
+                {
+                    Engine = new Engine
+                    {
+                        EngineType = EngineTypeEnum.Electric,
+                        Power = 50,
+                        Volume = 0,
+                        SerialNumber = "EL12345"
+                    },
+
+                    Chassis = new Chassis
+                    {
+                        WeightLoad = 100,
+                        WheelsCount = 2,
+                        SerialNumber = "SC12345"
+                    },
+
+                    Transmission = new Transmission
+                    {
+                        GearsCount = 0,
+                        TransmissionType = TransmissionTypeEnum.CVT,
+                        Manufacturer = "Jatco"
+                    }
+                },
+            };
+        }
+
+        private static List<Bus> GetBuses()
+        {
+            return new List<Bus>
+            {
+                new Bus
+                {
+                    Engine = new Engine
+                    {
+                        EngineType = EngineTypeEnum.Diesel,
+                        Power = 200,
+                        Volume = 3,
+                        SerialNumber = "D12345"
+                    },
+
+                    Chassis = new Chassis
+                    {
+                        WeightLoad = 5000,
+                        WheelsCount = 6,
+                        SerialNumber = "B12345"
+                    },
+
+                    Transmission = new Transmission
+                    {
+                        GearsCount = 6,
+                        TransmissionType = TransmissionTypeEnum.Automatic,
+                        Manufacturer = "Ford"
+                    }
+                },
+            };
+        }
+
+        private static List<Cargo> GetCargos()
+        {
+            return new List<Cargo>
+            {
+                new Cargo
+                {
+                    Engine = new Engine
+                    {
+                        EngineType = EngineTypeEnum.Petrol,
+                        Volume = 6,
+                        Power = 425,
+                        SerialNumber = "MAN12345"
+                    },
+
+                    Chassis = new Chassis
+                    {
+                        WeightLoad = 12000,
+                        WheelsCount = 8,
+                        SerialNumber = "CH12345"
+                    },
+
+                    Transmission = new Transmission
+                    {
+                        GearsCount = 6,
+                        TransmissionType = TransmissionTypeEnum.Manual,
+                        Manufacturer = "GM"
+                    }
+                },
+            };
+        }
+
+        private static List<PassengerCar> GetCars()
+        {
+            return new List<PassengerCar>
+            {
+                new PassengerCar
+                {
+                    Engine = new Engine
+                    {
+                        EngineType = EngineTypeEnum.Diesel,
+                        Power = 125,
+                        Volume = 1.5M,
+                        SerialNumber = "DS12345"
+                    },
+
+                    Chassis = new Chassis
+                    {
+                        WeightLoad = 1200,
+                        WheelsCount = 4,
+                        SerialNumber = "F12345"
+                    },
+
+                    Transmission = new Transmission
+                    {
+                        GearsCount = 5,
+                        Manufacturer = "Jatco",
+                        TransmissionType = TransmissionTypeEnum.Automatic
+                    }
+                },
+            };
+        }
     }
 }
